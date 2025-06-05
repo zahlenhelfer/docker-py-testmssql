9fed434adb1048763856b501a656d403d8a3089e
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-06-05 13:37:53 +0200

    Update test_mssql_connection.py

--- test_mssql_connection.py
+++ test_mssql_connection.py
@@ -6,7 +6,7 @@ server = os.getenv("MSSQL_SERVER")
 database = os.getenv("MSSQL_DB")
 username = os.getenv("MSSQL_USER")
 password = os.getenv("MSSQL_PASS")
-driver = '{ODBC Driver 17 for SQL Server}'
+driver = '{ODBC Driver 18 for SQL Server}'
 
 try:
     conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
