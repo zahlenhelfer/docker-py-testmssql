42ba555470157dcc1378a214a9b55e1e17d60cd9
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-06-05 13:41:25 +0200

    Update test_mssql_connection.py

--- test_mssql_connection.py
+++ test_mssql_connection.py
@@ -9,7 +9,7 @@ password = os.getenv("MSSQL_PASS")
 driver = '{ODBC Driver 18 for SQL Server}'
 
 try:
-    conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    conn_str = f'DRIVER={driver};TrustServerCertificate=yes;SERVER={server};DATABASE={database};UID={username};PWD={password}'
     conn = pyodbc.connect(conn_str, timeout=5)
     print("Connection successful!")
     conn.close()
